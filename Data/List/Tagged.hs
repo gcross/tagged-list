01d00e794241e0fb87dba7d6d6765d296b76b44c
Gregory Crosswhite <gcross@phys.washington.edu>
2010-09-29 17:00:56 -0700

    Added functionality for converting tagged lists to and from tuples.

--- Data/List/Tagged.hs
+++ Data/List/Tagged.hs
@@ -10,6 +10,8 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE UnicodeSyntax #-}
 -- @-node:gcross.20100918210837.1285:<< Language extensions >>
 -- @nl
@@ -201,6 +203,79 @@ zipf E E = E
 zipf (f :. fs) (x :. xs) = f x :. zipf fs xs
 -- @-node:gcross.20100918210837.1311:zipf
 -- @-node:gcross.20100918210837.1296:Functions
+-- @+node:gcross.20100929163524.1296:Tuple conversions
+type family TupleOf n α
+type instance TupleOf N0  α = ()
+type instance TupleOf N1  α = (α)
+type instance TupleOf N2  α = (α,α)
+type instance TupleOf N3  α = (α,α,α)
+type instance TupleOf N4  α = (α,α,α,α)
+type instance TupleOf N5  α = (α,α,α,α,α)
+type instance TupleOf N6  α = (α,α,α,α,α,α)
+type instance TupleOf N7  α = (α,α,α,α,α,α,α)
+type instance TupleOf N8  α = (α,α,α,α,α,α,α,α)
+type instance TupleOf N9  α = (α,α,α,α,α,α,α,α,α)
+type instance TupleOf N10 α = (α,α,α,α,α,α,α,α,α,α)
+type instance TupleOf N11 α = (α,α,α,α,α,α,α,α,α,α,α)
+type instance TupleOf N12 α = (α,α,α,α,α,α,α,α,α,α,α,α)
+type instance TupleOf N13 α = (α,α,α,α,α,α,α,α,α,α,α,α,α)
+type instance TupleOf N14 α = (α,α,α,α,α,α,α,α,α,α,α,α,α,α)
+type instance TupleOf N15 α = (α,α,α,α,α,α,α,α,α,α,α,α,α,α,α)
+
+class TupleConvertable n where
+    fromT :: TupleOf n α → TaggedList n α
+    toT :: TaggedList n α → TupleOf n α
+
+instance TupleConvertable N0 where
+    fromT _ = E
+    toT _ = ()    
+instance TupleConvertable N1 where
+    fromT (x1) = x1:.E
+    toT (x1:.E) = (x1)
+instance TupleConvertable N2 where
+    fromT (x1,x2) = x1:.x2:.E
+    toT (x1:.x2:.E) = (x1,x2)
+instance TupleConvertable N3 where
+    fromT (x1,x2,x3) = x1:.x2:.x3:.E
+    toT (x1:.x2:.x3:.E) = (x1,x2,x3)
+instance TupleConvertable N4 where
+    fromT (x1,x2,x3,x4) = x1:.x2:.x3:.x4:.E
+    toT (x1:.x2:.x3:.x4:.E) = (x1,x2,x3,x4)
+instance TupleConvertable N5 where
+    fromT (x1,x2,x3,x4,x5) = x1:.x2:.x3:.x4:.x5:.E
+    toT (x1:.x2:.x3:.x4:.x5:.E) = (x1,x2,x3,x4,x5)
+instance TupleConvertable N6 where
+    fromT (x1,x2,x3,x4,x5,x6) = x1:.x2:.x3:.x4:.x5:.x6:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.E) = (x1,x2,x3,x4,x5,x6)
+instance TupleConvertable N7 where
+    fromT (x1,x2,x3,x4,x5,x6,x7) = x1:.x2:.x3:.x4:.x5:.x6:.x7:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.x7:.E) = (x1,x2,x3,x4,x5,x6,x7)
+instance TupleConvertable N8 where
+    fromT (x1,x2,x3,x4,x5,x6,x7,x8) = x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.E) = (x1,x2,x3,x4,x5,x6,x7,x8)
+instance TupleConvertable N9 where
+    fromT (x1,x2,x3,x4,x5,x6,x7,x8,x9) = x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.E) = (x1,x2,x3,x4,x5,x6,x7,x8,x9)
+instance TupleConvertable N10 where
+    fromT (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10) = x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.E) = (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10)
+instance TupleConvertable N11 where
+    fromT (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11) = x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.E) = (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11)
+instance TupleConvertable N12 where
+    fromT (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12) = x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.x12:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.x12:.E) = (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12)
+instance TupleConvertable N13 where
+    fromT (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13) = x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.x12:.x13:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.x12:.x13:.E) = (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13)
+instance TupleConvertable N14 where
+    fromT (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14) = x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.x12:.x13:.x14:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.x12:.x13:.x14:.E) = (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14)
+instance TupleConvertable N15 where
+    fromT (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15) = x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.x12:.x13:.x14:.x15:.E
+    toT (x1:.x2:.x3:.x4:.x5:.x6:.x7:.x8:.x9:.x10:.x11:.x12:.x13:.x14:.x15:.E) = (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15)
+-- @nonl
+-- @-node:gcross.20100929163524.1296:Tuple conversions
 -- @-others
 -- @-node:gcross.20100917213812.1278:@thin Data/List/Tagged.hs
 -- @-leo
